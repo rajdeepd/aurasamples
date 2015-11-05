5d85e14960903436efc2dd2254d77b08918b2aa5
Rajdeep Dua <dua_rajdeep@yahoo.com>
2015-11-05 16:04:06 +0530

    Added sample for toggleCss

--- src/main/webapp/WEB-INF/components/docSample/clickEventsExternalJS/clickEventsExternalJSController.js
+++ src/main/webapp/WEB-INF/components/docSample/clickEventsExternalJS/clickEventsExternalJSController.js
@@ -2,10 +2,7 @@
 	getInput : function(cmp, evt) {
 		var myName = cmp.find("name").get("v.value");
 		var greet = getGreetings(myName);
-		//var myName = cmp.find("name").get("v.value");
-		var myText = cmp.find("outName");
-		//var greet = "Hi, " + myName;
-		
+		var myText = cmp.find("outName"); 
 		myText.set("v.value", greet);
 }
 })
