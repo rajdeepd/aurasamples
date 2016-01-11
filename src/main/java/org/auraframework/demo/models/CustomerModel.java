d4f694ac7eec967d0af9d3a769d99821ef69cf64
Rajdeep Dua <dua_rajdeep@yahoo.com>
2016-01-11 15:32:48 +0530

    CustomerModel

--- src/main/java/org/auraframework/demo/models/CustomerModel.java
+++ src/main/java/org/auraframework/demo/models/CustomerModel.java
@@ -8,13 +8,13 @@ import org.auraframework.system.Annotations.Model;
 
 @Model
 public class CustomerModel {
-	//String[] customersStr = {"John Doe", "Mike Getting"};
 	private List<String> customers;
 	public CustomerModel() {
 		customers = new LinkedList<String>();
 		customers.add("John Doe");
 		customers.add("Mark Heynman");
 		customers.add("Raj Shekhar");
+		customers.add("Shiv");
 	}
 
 	@AuraEnabled
